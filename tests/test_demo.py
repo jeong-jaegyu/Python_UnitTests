fcb2061aee995e79a6e63c28f03668d7ee06b047
jaegyu <TylenolWasTaken@protonmail.com>
2022-09-27 19:14:04 -0700

    init commit

---
+++ tests/test_demo.py
@@ -0,0 +1,23 @@
+import pytest
+
+from testdemo.testdemo import print_hi
+
+
+def test_demo():
+    print_hi("hi")
+
+
+@pytest.mark.skip(reason='reason here')
+def test_skip():
+    pass
+
+
+@pytest.mark.xfail
+def test_failure():
+    assert 1 == 2
+
+
+# Fails if error not raised
+def test_exp_err():
+    with pytest.raises(TypeError):
+        return "hello" * 4
